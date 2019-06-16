f49fd13bb96ebefe4fe9c90bdc29d317e3d67cf1
Anders <anders@andersnoren.se>
2019-06-16 10:10:46 +0200

    Added template: only content

---
+++ template-only-content.php
@@ -0,0 +1,45 @@
+<?php
+
+/* 
+Template Name: Only Content 
+*/
+
+get_header(); ?>
+
+<main id="site-content">
+
+	<?php
+
+	if ( have_posts() ) :
+
+		while ( have_posts() ) : the_post(); ?>
+
+			<article <?php post_class( 'section-inner' ); ?> id="post-<?php the_ID(); ?>">
+
+				<?php the_title( '<h1 class="screen-reader-text">', '</h1>' ); ?>
+
+				<div class="post-inner" id="post-inner">
+
+					<div class="entry-content">
+
+						<?php 
+						the_content();
+						wp_link_pages();
+						?>
+
+					</div><!-- .entry-content -->
+
+				</div><!-- .post-inner -->
+
+			</article><!-- .post -->
+			<?php
+
+		endwhile;
+
+	endif;
+
+	?>
+
+</main><!-- #site-content -->
+
+<?php get_footer(); ?>
